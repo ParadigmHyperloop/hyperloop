739c37841e22bdfebbc2103e92d6f19d46baab20
Thomas Hay <thomashay@Thomass-MacBook-Pro.local>
2016-10-08 01:30:08 -0400

    basic lateral.c

--- lateral.c
+++ lateral.c
@@ -1,1 +1,22 @@
 #include "pod.h"
+#include "main.c"
+
+long maxSafeDistanceAwayFromRail = 7; //CHANGE ME!! ARBITRARY In mm
+long minSafeDistanceAwayFromRail = 5; //CHANGE ME!! ARBITRARY In mm
+
+void checkTolerance(long distance) {
+    if(distance > maxSafeDistanceAwayFromRail || distance < minSafeDistanceAwayFromRail) {
+        //CHANGE ME! Change state to Emergency!
+    }
+}
+
+void *lateralMain(void *arg) {
+    long leftLateral;
+    long rightLateral;
+    while(1) {
+        leftLateralDistance = 0; //CHANGE ME! Read in value assume units in mm
+        rightLateralDistance = 0; //CHANGE ME! Read in value assume units in mm
+        checkTolerance(leftLateralDistance);
+        checkTolerance(rightLateralDistance);
+    }
+}
