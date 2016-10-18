103c75b16121068f5852d0faf69c9144c6888f84
Eddie Hurtig <eddie@hurtigtechnologies.com>
2016-10-18 01:06:14 -0400

    Add Proprietary License Headers Everywhere

--- ods/server.py
+++ ods/server.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python2.7
+##
+# Copyright (c) OpenLoop, 2016
+#
+# This material is proprietary of The OpenLoop Alliance and its members.
+# All rights reserved.
+# The methods and techniques described herein are considered proprietary
+# information. Reproduction or distribution, in whole or in part, is forbidden
+# except by express written permission of OpenLoop.
+#
+# Source that is published publicly is for demonstration purposes only and
+# shall not be utilized to any extent without express written permission of
+# OpenLoop.
+#
+# Please see http://www.opnlp.co for contact information
+##
+
 import os
 from datetime import datetime
 import logging
