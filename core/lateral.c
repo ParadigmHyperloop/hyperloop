24a28b2a71a430b674bbe302e0471832d9ee811e
Eddie Hurtig <eddie@hurtigtechnologies.com>
2016-10-18 18:01:52 -0400

    Fix typo in header licenses

--- core/lateral.c
+++ core/lateral.c
@@ -4,8 +4,8 @@
  * This material is proprietary of The OpenLoop Alliance and its members.
  * All rights reserved.
  * The methods and techniques described herein are considered proprietary
- * information. Reproduction or distribution, in whole or in part, is forbidden
- * except by express written permission of OpenLoop.
+ * information. Reproduction or distribution, in whole or in part, is
+ * forbidden except by express written permission of OpenLoop.
  *
  * Source that is published publicly is for demonstration purposes only and
  * shall not be utilized to any extent without express written permission of
