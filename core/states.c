d2fb67e0cc81bee700339d872e0842c38b7c41ee
Eddie Hurtig <eddie@hurtigtechnologies.com>
2017-03-24 18:57:14 -0700

    remove states-internal.c

--- core/states.c
+++ core/states.c
@@ -35,6 +35,48 @@
 
 bool validate_transition(pod_mode_t current_mode, pod_mode_t new_mode);
 
+/**
+ * Determines if the new mode is a valid mode
+ *
+ * @return whether the new mode is valid knowing the gPodState
+ */
+bool validate_transition(pod_mode_t current_mode, pod_mode_t new_mode) {
+  const static pod_mode_t transitions[N_POD_STATES][N_POD_STATES + 1] = {
+    {POST, Boot, Emergency, NonState},
+    {Boot, LPFill, Emergency, NonState},
+    {LPFill, HPFill, Emergency, NonState},
+    {HPFill, Load, Emergency, NonState},
+    {Load, Standby, Emergency, NonState},
+    {Standby, Load, Armed, Emergency, NonState},
+    {Armed, Standby, Pushing, Emergency, NonState},
+    {Pushing, Coasting, Braking, Emergency, NonState},
+    {Coasting, Braking, Pushing, Emergency, NonState},
+    {Braking, Pushing, Vent, Emergency, NonState},
+    {Vent, Retrieval, Emergency, NonState},
+    {Retrieval, Shutdown, NonState},
+    {Emergency, Vent, NonState},
+    {Shutdown, NonState},
+  };
+  
+  // Ensure that the pod's current state can always transition to itself
+  assert(transitions[current_mode][0] == current_mode);
+  
+  pod_mode_t i_state;
+  // Do not include Current Mode => Same Current Mode
+  int i = 1;
+  
+  while ((i_state = transitions[current_mode][i]) != NonState) {
+    // debug("Checking %s == %s", pod_mode_names[i_state],
+    // pod_mode_names[new_mode]);
+    if (i_state == new_mode) {
+      return true;
+    }
+    i++;
+  }
+  
+  return false;
+}
+
 char *pod_mode_names[N_POD_STATES] = {
     "POST",    "Boot",      "LPFill",    "HPFill",   "Load",
     "Standby", "Armed",     "Pushing",   "Coasting", "Braking",
