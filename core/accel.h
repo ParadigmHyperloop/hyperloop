531adb293598a7759b84c9b5a928d2291ad77379
DEAD P001 <deadp001@paradigmhyperloop.com>
2017-04-24 21:04:04 -0700

    Add accel.h

---
+++ core/accel.h
@@ -0,0 +1,46 @@
+/*****************************************************************************
+ * Copyright (c) Paradigm Hyperloop, 2017
+ *
+ * This material is proprietary intellectual property of Paradigm Hyperloop.
+ * All rights reserved.
+ *
+ * The methods and techniques described herein are considered proprietary
+ * information. Reproduction or distribution, in whole or in part, is
+ * forbidden without the express written permission of Paradigm Hyperloop.
+ *
+ * Please send requests and inquiries to:
+ *
+ *  Software Engineering Lead - Eddie Hurtig <hurtige@ccs.neu.edu>
+ *
+ * Source that is published publicly is for demonstration purposes only and
+ * shall not be utilized to any extent without express written permission of
+ * Paradigm Hyperloop.
+ *
+ * Please see http://www.paradigm.team for additional information.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PARADIGM HYPERLOOP ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL PARADIGM HYPERLOOP BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ****************************************************************************/
+
+#ifndef PARADIGM_ACCEL_H
+#define PARADIGM_ACCEL_H
+
+/**
+ * Computes the pod's new position, velocity, and acceleration vectors given
+ * the IMU datagram
+ *
+ * @param data A pointer to the IMU datagram containing the new data
+ * @param s A pointer to the pod structure to update
+ */
+void add_imu_data(imu_datagram_t *data, pod_t *s);
+
+
+#endif /* PARADIGM_ACCEL_H */
