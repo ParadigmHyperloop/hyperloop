f192fc38dd9691d6250d2487cd302c4cda4634d9
Dan Hartman <daniel.hartman4761@gmail.com>
2017-10-31 18:21:34 -0400

    Revert "Update Debug scripts for new build system"

--- remote_run.sh
+++ remote_run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-: "${BBB_USER:=root}"
-: "${BBB_HOST:=192.168.0.10}"
-: "${BBB_FOLDER:=~/hyperloop}"
 
-
-ssh "$BBB_USER@$BBB_HOST" "bash -c 'cd $BBB_FOLDER && ./run_core.sh'"
+ssh root@192.168.0.10 'bash -c "cd hyperloop && make run"'
